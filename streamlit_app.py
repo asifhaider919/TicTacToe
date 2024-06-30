c978ec1897328881aa0b37c4651dfd28bd4b775d
asifhaider919 <asifhaider.rno@gmail.com>
2024-06-30 08:08:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,154 +1,55 @@
 import streamlit as st
-import pandas as pd
-import folium
-from streamlit_folium import folium_static
 
-# Set page configuration
-st.set_page_config(layout="wide")
+# Initialize the game board
+if 'board' not in st.session_state:
+    st.session_state.board = [' ' for _ in range(9)]
+    st.session_state.current_player = 'X'
+    st.session_state.winner = None
+    st.session_state.moves = 0
 
-# Logo image URL (replace with your actual logo URL)
-logo_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Kj80VCFDZV3eFqa8ppMxXlhxvjkr6XQ85A&s"
+# Define the winning combinations
+winning_combinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
+    [0, 4, 8], [2, 4, 6]  # Diagonal
+]
 
-# Display the logo at the top of the sidebar
-st.sidebar.image(logo_url, width=200)
+# Function to check for a winner
+def check_winner():
+    for combo in winning_combinations:
+        if st.session_state.board[combo[0]] == st.session_state.board[combo[1]] == st.session_state.board[combo[2]] != ' ':
+            st.session_state.winner = st.session_state.current_player
+            return True
+    return False
+
+# Function to handle a move
+def make_move(index):
+    if st.session_state.board[index] == ' ' and st.session_state.winner is None:
+        st.session_state.board[index] = st.session_state.current_player
+        st.session_state.moves += 1
+        if check_winner():
+            st.success(f"Player {st.session_state.current_player} wins!")
+        elif st.session_state.moves == 9:
+            st.warning("It's a tie!")
+        else:
+            st.session_state.current_player = 'O' if st.session_state.current_player == 'X' else 'X'
+
+# Function to reset the game
+def reset_game():
+    st.session_state.board = [' ' for _ in range(9)]
+    st.session_state.current_player = 'X'
+    st.session_state.winner = None
+    st.session_state.moves = 0
 
 # Title of the app
-# st.title("Site and Transaction Map")
+st.title("Tic Tac Toe")
 
-# Sidebar for file upload
-uploaded_file_site = st.sidebar.file_uploader("Upload Site Info file", type=["xlsx"])
-uploaded_file_txn = st.sidebar.file_uploader("Upload TXN Info file", type=["xlsx"])
+# Display the game board
+cols = st.columns(3)
+for i in range(3):
+    for j in range(3):
+        index = i * 3 + j
+        cols[j].button(st.session_state.board[index], key=index, on_click=make_move, args=(index,))
 
-if uploaded_file_site is not None and uploaded_file_txn is not None:
-    try:
-        # Read the uploaded site file into a pandas DataFrame
-        if uploaded_file_site.name.endswith('.xls') or uploaded_file_site.name.endswith('.xlsx'):
-            site_data = pd.read_excel(uploaded_file_site)
-        else:
-            site_data = pd.read_csv(uploaded_file_site)
-        
-        # Ensure the required columns are present for site data
-        if 'Lat' not in site_data.columns or 'Lon' not in site_data.columns or 'Site' not in site_data.columns:
-            st.sidebar.error("The uploaded site file must contain 'Site', 'Lat', and 'Lon' columns.")
-        else:
-            # Define categories for the legend based on 'Issue' column
-            site_categories = site_data['Issue'].unique().tolist()
-																											   
-            # Extend colors list to accommodate up to 10 categories
-            colors = ['green', 'blue', 'red', 'purple', 'orange', 'black', 'magenta', 'yellow', 'lime', 'teal']
-
-            # Assign light green to a specific category
-            # Example: Assign 'lightgreen' to the category 'OK'
-            colors[site_categories.index('OK')] = 'green'
-		
-            # Sidebar filter by Site Name
-            search_site_name = st.sidebar.text_input("Enter Site Name")
-
-            # Create initial map centered around the mean location of all site data
-            combined_map = folium.Map(location=[site_data['Lat'].mean(), site_data['Lon'].mean()], zoom_start=7)
-
-            # Display markers for filtered site data or all site data if not filtered
-            if search_site_name:
-                filtered_site_data = site_data[site_data['Site'].str.contains(search_site_name, case=False)]
-                if not filtered_site_data.empty:
-                    # Calculate bounds to zoom to 10km around the first filtered site
-                    first_site = filtered_site_data.iloc[0]
-                    bounds = [(first_site['Lat'] - 0.05, first_site['Lon'] - 0.05), 
-                              (first_site['Lat'] + 0.05, first_site['Lon'] + 0.05)]
-                    
-                    for idx, row in site_data.iterrows():
-                        # Determine marker size
-                        radius = 12 if row['Site'] in filtered_site_data['Site'].values else 6
-
-                        # Determine marker color based on 'Issue' category
-                        category = row['Issue']
-                        color = colors[site_categories.index(category) % len(colors)]
-
-                        # Create a popup message with site information
-                        popup_message = f"<b>Site Name:</b> {row.get('Site', '')}<br>" \
-                                    	f"<b>SITECODE:</b> {row['SITECODE']}<br>" \
-                                    	f"<b>CONFIG:</b> {row['CONFIG']}<br>" \
-                                    	f"<b>Longitude:</b> {row['Lon']}<br>" \
-                                    	f"<b>Latitude:</b> {row['Lat']}<br>" \
-                                    	f"<b>Issue:</b> {row['Issue']}<br>"
-
-                        folium.CircleMarker(
-                            location=[row['Lat'], row['Lon']],
-                            radius=radius,
-                            color=color,
-                            fill=True,
-                            fill_color=color,
-                            fill_opacity=0.7,
-                            popup=folium.Popup(popup_message, max_width=400)
-                        ).add_to(combined_map)
-                    
-                    # Fit the map to the bounds
-                    combined_map.fit_bounds(bounds)
-            else:
-                for idx, row in site_data.iterrows():
-                    # Determine marker color based on 'Issue' category
-                    category = row['Issue']
-                    color = colors[site_categories.index(category) % len(colors)] if category in site_categories else 'blue'
-
-                    # Create a popup message with site information
-                    popup_message = f"<b>Site Name:</b> {row.get('Site', '')}<br>" \
-                                    f"<b>SITECODE:</b> {row['SITECODE']}<br>" \
-                                    f"<b>CONFIG:</b> {row['CONFIG']}<br>" \
-                                    f"<b>Longitude:</b> {row['Lon']}<br>" \
-                                    f"<b>Latitude:</b> {row['Lat']}<br>" \
-                                    f"<b>Issue:</b> {row['Issue']}<br>"
-
-                    folium.CircleMarker(
-                        location=[row['Lat'], row['Lon']],
-                        radius=6,
-                        color=color,
-                        fill=True,
-                        fill_color=color,
-                        fill_opacity=0.7,
-                        popup=folium.Popup(popup_message, max_width=400)
-                    ).add_to(combined_map)
-
-            # Display the legend for site data in the sidebar with colored checkboxes
-            st.sidebar.subheader("Site Map Legend")
-            for idx, category in enumerate(site_categories):
-                color = colors[idx % len(colors)]  # Get color for category
-                # Use HTML and CSS to create colored checkboxes
-                st.sidebar.markdown(f'<span style="color: {color}; font-size: 1.5em">&#9632;</span> {category}', unsafe_allow_html=True)
-				
-            # Read the uploaded TXN file into a pandas DataFrame
-            if uploaded_file_txn.name.endswith('.xls') or uploaded_file_txn.name.endswith('.xlsx'):
-                txn_data = pd.read_excel(uploaded_file_txn)
-            else:
-                txn_data = pd.read_csv(uploaded_file_txn)
-            
-            # Check for required columns for TXN data
-            required_columns_txn = ['Site_A', 'Site_B', 'Lat_A', 'Lon_A', 'Lat_B', 'Lon_B']
-            if all(col in txn_data.columns for col in required_columns_txn):
-                # Convert relevant columns to numeric (in case they are not already numeric)
-                numeric_columns_txn = ['Lat_A', 'Lon_A', 'Lat_B', 'Lon_B']
-                txn_data[numeric_columns_txn] = txn_data[numeric_columns_txn].apply(pd.to_numeric, errors='coerce')
-                
-                # Iterate through rows to draw lines for TXN data
-                for index, row in txn_data.iterrows():
-                    # Skip rows with missing or NaN values in coordinates
-                    if pd.isnull(row[['Lat_A', 'Lon_A', 'Lat_B', 'Lon_B']]).any():
-                        st.sidebar.warning(f"Skipping TXN row {index+1}: Missing coordinates")
-                        continue
-                    
-                    # Add a line from Lat_A/Lon_A to Lat_B/Lon_B
-                    folium.PolyLine(
-                        locations=[(row['Lat_A'], row['Lon_A']), (row['Lat_B'], row['Lon_B'])],
-                        color='#0000FF',  # Change color here
-                        weight=1  # Change line weight here
-                    ).add_to(combined_map)
-                
-
-                # Display the combined map in the Streamlit app
-                # st.subheader("Combined Site and Transaction Map")
-                folium_static(combined_map, width=1200, height=700)
-                
-            else:
-                st.sidebar.warning(f"Required columns {required_columns_txn} not found in the TXN file.")
-    
-    except Exception as e:
-        st.sidebar.error(f"An error occurred while processing the file: {e}")
+# Reset button
+st.button("Reset", on_click=reset_game)
